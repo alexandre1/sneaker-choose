ad5b9798e906cf85a45df60588585ab9c5b8363b
alexandre1 <alexjaquet@gmail.com>
2023-12-08 01:18:08 +0100

    Update MailWithAttachmentService.java
    Update TLS

--- src/main/java/org/vaadin/example/MailWithAttachmentService.java
+++ src/main/java/org/vaadin/example/MailWithAttachmentService.java
@@ -41,14 +41,16 @@ public class MailWithAttachmentService {
         final String password = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");  
-        
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.socketFactory.port", "587");    
         props.put("mail.smtp.socketFactory.class",    
                   "javax.net.ssl.SSLSocketFactory");    
         props.put("mail.smtp.auth", "true");    
         props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.starttls.enable", "true");
+
         Session session = Session.getInstance(props,
          new javax.mail.Authenticator() {
           protected PasswordAuthentication getPasswordAuthentication() {
